5382bbff3dfeb1f1c6d6734dbbe8519ecd5027f5
Freda <85437054+freda1874@users.noreply.github.com>
2023-07-27 14:39:21 -0400

    Electronic Health Records System
    Designed and implemented a Java-based Electronic Health Records system, utilizing object-oriented principles for optimal data management.

---
+++ w23assignment1/MyHealthDataTest2JuniTest.java
@@ -0,0 +1,14 @@
+ 
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MyHealthDataTest2JuniTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
